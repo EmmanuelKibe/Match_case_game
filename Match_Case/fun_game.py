06af8fafeb160bbd73aad629b140fd55739ac387
Emmanuel <emmanuelkibe148@gmail.com>
2025-05-29 14:36:51 +0300

    Added a while loop to keep prompting the user

--- Match_Case/fun_game.py
+++ Match_Case/fun_game.py
@@ -3,30 +3,35 @@ import random
 # Generate random number from 1 to 10
 secret_number = random.randint(1, 10)
 
-# Get the user's guess
-guess = int(input("I'm thinking of a number from 1 to 10. Can you guess what it is? "))
+while True:
+    try:
+        # Get the user's guess
+        guess = int(input("I'm thinking of a number from 1 to 10. Can you guess what it is? "))
 
-# Validate input before matching
-if guess < 1 or guess > 10:
-    print("Invalid input. Please enter a number between 1 and 10.")
-else:
-    # Match case block for valid input
-    match guess:
-        #If the user gets it right
-        case _ if guess == secret_number:
-            print("Congratulations, you guessed it!")
+        # Validate input range
+        if guess < 1 or guess > 10:
+            print("Invalid input. Please enter a number between 1 and 10.")
+            continue  # Ask again
 
-            #Ask if the user wants to play again
-            play_again = input("Do you want to play again? (yes/no): ").strip().lower()
-            if play_again == 'yes' :
-                print(". . . (game restarts)")
-            else:
-                print("Thanks for playing!")
+        # Match case block for valid input
+        match guess:
+            case _ if guess == secret_number:
+                print("Congratulations, you guessed it!")
 
-        # If the guess is too low 
-        case _ if guess < secret_number:
-            print("Nope, your guess is too low. Try again.")
+                # Ask if the user wants to play again
+                play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+                if play_again == 'yes':
+                    secret_number = random.randint(1, 10)  # Generate a new number
+                    continue
+                else:
+                    print("Thanks for playing!")
+                    break  # Exit the loop
 
-        # If the guess is too high
-        case _ if guess > secret_number:
-            print("Oops, your guess is too high. Try again.")
+            case _ if guess < secret_number:
+                print("Nope, your guess is too low. Try again.")
+
+            case _ if guess > secret_number:
+                print("Oops, your guess is too high. Try again.")
+
+    except ValueError:
+        print("Please enter a valid number.")
